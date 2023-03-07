ef81897a630c79f4ad22c9e9e418150ae7b88926
send_it_2_me_ <send_it_2_me_@hotmail.com>
2023-03-07 20:28:38 +0000

    do less

--- mirror/start.sh
+++ mirror/start.sh
@@ -4,20 +4,4 @@ script_name=$0
 script_full_path=$(dirname "$0")
 cd $script_full_path
 
-cd ..
-sed -i -e "s|CURRENTUSERID|$(id -u)|" -e "s|CURRENTUSERGID|$(id -u)|" ./Dockerfile
-docker build -t mirror_meuse .
-cd mirror
-
-sed -e "s|CURRENTUSERID|$(id -u)|" -e "s|CURRENTUSERGID|$(id -u)|" ./docker-compose-base.yaml > ./docker-compose.yaml
-
-if [[ ! -d ./registry ]]; then
-    git clone git@github.com:sendit2me/crates.io-index.git registry
-else
-    git pull
-fi
-if [[ ! -d ./crates ]]; then
-    mkdir -p ./crates
-fi
-
 docker-compose -f docker-compose.yaml up -d
